649ac6d453384ca740a91e5c6aa01e7cba5daaa1
estelaclara <estelaaclaraa@gmail.com>
2024-05-24 16:38:20 -0300

    login cadastro finalizado

--- login-cadastro/app.js
+++ login-cadastro/app.js
@@ -35,23 +35,60 @@ app.post ('/login', (req,res)=> {
     const username = req.body.usuario
     const password = req.body.senha
 
-    db.query('select password from user where username = ?', [username] , (error, results)=>{ 
+    db.query('SELECT password from user where username = ?', [username] , (error, results)=>{ 
         if(error){
             console.log('Erro ao realizar consulta', error);
         }else{
             if(results.length > 0 ){
                 const passwordBD = results[0].password;
                 if (passwordBD === password){
-                    console.log('Login bem sucedido!')
+                    console.log('Login Bem Sucedido!')
     
                 }else{
-                    console.log('Login ou senha incorretos')
+                    console.log('Login ou Senha Incorretos')
                 }
            }else{
-            console.log('Usuario não cadastrado')
+            console.log('Usuário Não Cadastrado')
            } 
         }
      })
     
 
 });
+
+app.get("/cadastro", (req, res)=> {
+    res.sendFile(__dirname + '/views/cadastro.html')
+})
+
+    
+app.post("/cadastro", (req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const confirm = req.body.passwordConfirm;
+
+
+    if (password === confirm) {
+        db.query('insert into user (username, password) values (?, ?)', [username, password], (error, results) =>{
+            if(error){
+                console.log("Erro ao realizar o cadastro", error)
+            } else {
+                console.log("Cadastro realizado com sucesso!")
+            }
+        })
+
+    }else {
+        console.log("Senhas Não Coincidem")
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
